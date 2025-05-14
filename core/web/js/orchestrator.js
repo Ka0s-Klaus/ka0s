bb429576bf160331556bc2067db30c23c29440fc
jorgecg646 <jorgecg646@gmail.com>
2025-05-14 10:28:07 +0200

    Lista configurada

--- core/web/js/orchestrator.js
+++ core/web/js/orchestrator.js
@@ -255,6 +255,9 @@ function initNavbar() {
     // Cargar las secciones
     const navbar = document.getElementById('navbar-sections');
     if (navbar && webConfig.sections && Array.isArray(webConfig.sections)) {
+        // Limpiar elementos existentes para evitar duplicados
+        navbar.innerHTML = '';
+        
         webConfig.sections.forEach(section => {
             const li = document.createElement('li');
             li.innerHTML = `
@@ -264,9 +267,6 @@ function initNavbar() {
                         <i class="fas ${section.icon} text-orange-300 group-hover:text-orange-500 text-xl"></i>
                     </div>
                     <span class="sidebar-text flex-1 font-medium capitalize">${section.title}</span>
-                    <div class="tooltip hidden absolute left-16 top-1/2 -translate-y-1/2 bg-gray-800 text-white px-2 py-1 rounded text-sm whitespace-nowrap opacity-0 pointer-events-none group-hover:opacity-100 transition-opacity duration-200">
-                        ${section.title}
-                    </div>
                 </a>`;
             navbar.appendChild(li);
         });
@@ -351,6 +351,7 @@ function initDataList() {
     } else {
         loadData(config.defaultArchive);
     }
+    console.log("data-list element:", dataListElement);
 }
 
 // Cargar datos desde una fuente específica
@@ -381,7 +382,7 @@ function loadData(dataSource) {
 
             // Inicializar con todos los datos
             filteredData = [...allData];
-
+            console.log("data:", data);
             // Renderizar la primera página
             renderPage(1);
         },
@@ -401,6 +402,7 @@ function loadData(dataSource) {
 
 // Renderizar una página de datos
 function renderPage(page) {
+    console.log("pagina: ", page);
     currentPage = page;
     const startIndex = (page - 1) * config.itemsPerPage;
     const endIndex = startIndex + config.itemsPerPage;
@@ -428,6 +430,7 @@ function renderPage(page) {
     if (pageData.length > 0) {
         const dataHeaders = document.getElementById('data-headers');
         if (!dataHeaders) return;
+        console.log("datalistelement:", dataList);  // Cambiado de dataListElement a dataList
 
         // Limpiar cualquier clase de scroll anterior
         if (dataHeaders.parentElement) {
