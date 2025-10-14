19bb9f0ccccb9924ec24d96274fe629beb31328b
asantacana1970 <asantacana@kyndryl.com>
2025-10-14 15:41:04 +0200

    del portal and recreated

--- core/portal/portal/upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CONFIGS=(app-config.yaml app-config.production.yaml catalog-info.yaml backstage.json)
-CONFIG_DIR="core/portal/portal"
-for file in "${CONFIGS[@]}"; do
-  config_name="$(echo "$file" | tr . _ | tr -d -)"
-  docker config create "$config_name" "$CONFIG_DIR/$file"
-done
